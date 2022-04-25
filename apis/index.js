a0092a80c2c57a123c97b3393d4dd06afe4d580e
Paulo Oyama <papaloyama@gmail.com>
2022-04-25 18:25:51 -0300

    Povoando os arquivos .json

--- apis/index.js
+++ apis/index.js
@@ -3,11 +3,13 @@ const regulation = require('./regulation.json');
 const orders = require('./orders.json');
 const saved = require('./saved.json');
 const supplier = require('./supplier-historic.json');
+const details = require('./details.json');
 
 module.exports = () => ({
-    recents: recents,
-    regulation: regulation,
+    recents: recents.recents,
+    regulation: regulation.items,
     orders: orders,
-    saved: saved,
-    supplier: supplier
+    saved: saved.saved,
+    supplier: supplier.supplier_historic,
+    details: details.data,
 });
